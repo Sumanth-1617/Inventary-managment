3bb9dad1e541469089a9f26acdee64a5e6d2fe54
Sumanth-1617 <muthyalasumanth8555@gmail.com>
2025-08-07 21:31:54 +0530

    Update inventary.sql

--- inventary.sql
+++ inventary.sql
@@ -1,4 +1,4 @@
 create database inventory;
 use inventory;
-create table inventory_system(id int auto_increment,name varchar(30),quantity int,price Double,primary key(id));
-select *from inventory_system;
+create table inventory_management(id int auto_increment,name varchar(30),quantity int,price Double,primary key(id));
+select *from inventory_management;
