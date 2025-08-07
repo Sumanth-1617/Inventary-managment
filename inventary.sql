2a2b40db0f48dd94f270ffda4c423caa849522d2
Sumanth-1617 <muthyalasumanth8555@gmail.com>
2025-08-07 21:29:33 +0530

    Add files via upload

---
+++ inventary.sql
@@ -0,0 +1,4 @@
+create database inventory;
+use inventory;
+create table inventory_system(id int auto_increment,name varchar(30),quantity int,price Double,primary key(id));
+select *from inventory_system;
